16f9bb58e10dc359eff8cac08c73ad6126b8e10d
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2018-09-11 23:42:28 -0300

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,14 +2,14 @@ CarrierWave.configure do |config|
   if Rails.env.development? || Rails.env.test?
     config.storage = :file
   else
-    config.fog_provider = 'fog/aws'                        # required
+    config.fog_provider = 'fog/aws'           # required
     config.fog_credentials = {
       provider: 'AWS',                        # required
-      aws_access_key_id: ENV['S3_ACCESS_KEY'],                        # required unless using use_iam_profile
-      aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required unless using use_iam_profile
+      aws_access_key_id: ENV['S3_ACCESS_KEY'],# required unless using use_iam_profile
+      aws_secret_access_key: ENV['S3_SECRET_KEY']# required unless using use_iam_profile
     }
-    config.fog_directory  = ENV['S3_BUCKET']                                      # required
-    config.fog_public     = false                                                 # optional, defaults to true
+    config.fog_directory  = ENV['S3_BUCKET']     # required
+    config.fog_public     = false                # optional, defaults to true
     config.storage = :fog
   end
 end
