a6365cce2303f34d93802e1e752bd0646629d439
sixto.guerra1982@gmail.com <sixto.guerra1982@gmail.com>
2018-09-11 22:37:20 -0300

    fog

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,11 +6,10 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',                        # required
       aws_access_key_id: ENV['S3_ACCESS_KEY'],                        # required unless using use_iam_profile
-      aws_secret_access_key: ENV['S3_SECRET_KEY']                        # required unless using use_iam_profile
+      aws_secret_access_key: ENV['S3_SECRET_KEY'],                        # required unless using use_iam_profile
     }
     config.fog_directory  = ENV['S3_BUCKET']                                      # required
     config.fog_public     = false                                                 # optional, defaults to true
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" } # optional, defaults to {}
     config.storage = :fog
   end
 end
